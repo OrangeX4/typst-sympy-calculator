cc2132f5b77dfa4c834057693c834a2f27eff7c3
OrangeX4 <318483724@qq.com>
2023-09-16 14:03:31 +0800

    bump version

--- TypstCalculatorServer.py
+++ TypstCalculatorServer.py
@@ -7,7 +7,7 @@ import os
 from TypstCalculator import TypstCalculator
 from DefaultTypstCalculator import get_default_calculator
 
-VERSION = '0.5.1'
+VERSION = '0.6.0'
 
 
 class TypstCalculatorServer:
